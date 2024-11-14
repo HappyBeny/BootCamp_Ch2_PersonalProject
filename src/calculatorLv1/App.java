8a16c4ae8df4e3dc63c409f7e9100d9e1c5a44c7
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-15 00:31:52 +0900

    계산기 Lv1 다시 구현 ㅠ

--- src/calculatorLv1/App.java
+++ src/calculatorLv1/App.java
@@ -5,8 +5,53 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
+        while(true) {
+            System.out.println("0 이상의 정수를 2개 입력해주세요");
+            System.out.print("첫 번째 정수 : ");
+            int firNum = sc.nextInt();
+            sc.nextLine();
+            System.out.print("두 번째 정수 : ");
+            int secNum = sc.nextInt();
+            sc.nextLine();
 
-        // calculate 메서드에 정수 2개, 연산기호 삽입
-        Calculator cal1 = new Calculator();
+            System.out.print("연산기호를 입력해주세요( +, -, *, / ) : ");
+            String sign = sc.nextLine();
+            System.out.println();
+
+            switch (sign) {
+                case ("+"):
+                    System.out.println("결과 : " + (firNum+secNum));
+                    System.out.println();
+                    break;
+                case ("-"):
+                    System.out.println("결과 : " + (firNum-secNum));
+                    System.out.println();
+                    break;
+                case ("*"):
+                    System.out.println("결과 : " + (firNum*secNum));
+                    System.out.println();
+                    break;
+                case ("/"):
+                    if (secNum == 0){
+                        System.out.println("숫자를 0으로 나누는 것은 불가능합니다. 다시 입력해주세요");
+                        System.out.println();
+                        continue;
+                    } else {
+                        System.out.println("결과 : " + ((double) firNum/secNum));
+                    }
+                default:
+                    System.out.println("잘못 입력하셨습니다. 다시 확인해주세요");
+                    System.out.println();
+                    break;
+            }
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료, 이외의 응답 입력 시 계속 진행) : ");
+            String answer = sc.nextLine();
+            if (answer.equalsIgnoreCase("exit")){
+                break;
+            }
+            System.out.println();
+        }
     }
 }
