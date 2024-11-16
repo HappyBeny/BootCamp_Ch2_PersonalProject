acfec9656b0258d1d366cf43029c015d4e7f2e45
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-16 17:11:21 +0900

    Lv2 계산기 구현중. 구조 짜ì디조 디테일하게 짜야 함

--- src/calculatorLv2_ref/Calculator.java
+++ src/calculatorLv2_ref/Calculator.java
@@ -1,4 +1,17 @@
 package calculatorLv2_ref;
 
 public class Calculator {
+    Operator operator;
+    Checker checker;
+
+    public Calculator(){
+        this.operator = new Operator() {
+            @Override
+            public double calculate() {
+                return 0;
+            }
+        };
+
+        this.checker = new Checker();
+    }
 }
